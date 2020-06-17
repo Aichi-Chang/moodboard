dee5f5251445de052a03bb7ae096a4dfd0f418db
Aichi Chang <phoebexxxc@gmail.com>
2020-06-17 12:23:46 +0100

    header

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `with Finn and Jack`,
+    description: `Find all episodes and characters`,
     author: `@gatsbyjs`,
   },
   plugins: [
@@ -15,6 +15,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,36 @@
-import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import { css } from '@emotion/core'
+
+import Image from './image'
+
+
+
 
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#32a5e3`,
       marginBottom: `1.45rem`,
     }}
   >
     <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
+      css={css`
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        margin: 0;
+        max-width: 500px;
+        padding: 1.45rem 1.0875rem;
+      `}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
+      <div style={{ width: '30%', marginRight: 10 }}>
+        <Image />
+      </div>
+      
+       <h1 style={{ margin: 0, color: `white`, fontSize: 20 }}>
           {siteTitle}
-        </Link>
-      </h1>
+      </h1> 
     </div>
   </header>
 )
